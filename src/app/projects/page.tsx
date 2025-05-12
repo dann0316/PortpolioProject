8e4bc60b728b52d5d00eb2808691abfd2a770abc
정다니엘 <cdl2141@naver.com>
2025-05-12 17:25:18 +0900

    fixed

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 
